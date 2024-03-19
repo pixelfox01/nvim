cae4092fa2bdd0368fd198df2d282898194585bf
Imran Yafith <imranyafith@gmail.com>
2024-03-19 18:19:38 -0400

    add linting(eslint, prettier, stylua)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup(
-      {
-        ensure_installed = {"lua", "javascript"},
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
-    )
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
