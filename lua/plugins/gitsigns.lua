e80b39faa3bae45ca650687909bdd5d8d19f696b
Imran Yafith <imranyafith@gmail.com>
2024-03-22 23:26:10 -0400

    update: gitsigns config to default styling

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,6 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
-	},
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup()
+  end,
 }
