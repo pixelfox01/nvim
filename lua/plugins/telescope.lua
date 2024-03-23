c619e82a419307baa24358fad1966bccb76e2b55
Imran Yafith <imranyafith@gmail.com>
2024-03-22 22:50:37 -0400

    update: keymap to search nvim config files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,36 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ct", builtin.colorscheme, {})
 
-  return {
-    {
-      "nvim-telescope/telescope.nvim", tag = "0.1.6",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      end
-    },
-    {
-      "nvim-telescope/telescope-ui-select.nvim",
-      config = function()
-        require("telescope").setup({
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {}
-            }
-          }
-        })
-        require("telescope").load_extension("ui-select")
-      end
-    }
-  }
+      -- Shortcut for searching your Neovim configuration files
+      vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
+      end, { desc = "[S]earch [N]eovim files" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
