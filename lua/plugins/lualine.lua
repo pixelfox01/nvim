7fb051750d662c634b91c15a7b1e8dac5896acbe
Imran Yafith <imranyafith@gmail.com>
2024-03-24 11:03:32 -0400

    add: noice and edit lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,29 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "jellybeans",
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_b = {
+					{
+						"buffers",
+					},
+				},
+				lualine_c = {},
+				lualine_x = {
+					{
+						"branch",
+						"diff",
+						"diagnostics",
+					},
+				},
+			},
+			options = {
+				theme = "jellybeans",
+				globalstatus = true,
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = "|",
+			},
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,50 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
-	},
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["ii"] = "@conditional.inner",
-						["ai"] = "@conditional.outer",
-						["il"] = "@loop.inner",
-						["al"] = "@loop.outer",
-					},
-				},
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "bash",
+        "c",
+        "c_sharp",
+        "cpp",
+        "css",
+        "dockerfile",
+        "python",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "gitignore",
+        "tmux",
+        "vim",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ii"] = "@conditional.inner",
+            ["ai"] = "@conditional.outer",
+            ["il"] = "@loop.inner",
+            ["al"] = "@loop.outer",
+          },
+        },
+      },
+    })
+  end,
 }
