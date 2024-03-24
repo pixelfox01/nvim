316d1304ba03b194b8d70cb8c49d6138213502ac
Imran Yafith <imranyafith@gmail.com>
2024-03-24 08:48:17 -0400

    add: vim-tmux-navigator and more textobjects in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,11 +15,16 @@ return {
 					enable = true,
 					lookahead = true,
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
 						["af"] = "@function.outer",
 						["if"] = "@function.inner",
 						["ac"] = "@class.outer",
 						["ic"] = "@class.inner",
+						["ii"] = "@conditional.inner",
+						["ai"] = "@conditional.outer",
+						["il"] = "@loop.inner",
+						["al"] = "@loop.outer",
 					},
 				},
 			},
