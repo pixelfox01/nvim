8698257f9ace659ef141f5ef38bd0c9d4c07c46b
Imran Yafith <imranyafith@gmail.com>
2024-04-29 02:54:04 -0400

    update: remove unused colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,5 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-			})
-		end,
-	},
-	{
-		"navarasu/onedark.nvim",
-		name = "onedark",
-		priority = 1000,
-		config = function()
-			require("onedark").setup({
-				style = "warmer",
-				code_style = {
-					variables = "italic",
-				},
-			})
-		end,
-	},
-	{
 		"sainnhe/gruvbox-material",
 		priority = 1000,
 		config = function()
